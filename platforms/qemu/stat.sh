8010f3c5067baf7220adbc3d2ee5c3438e1372fe
Mirko Krause <mirko.krause@student.hpi.de>
2018-12-17 13:20:31 +0100

    statted kvm

--- platforms/qemu/stat.sh
+++ platforms/qemu/stat.sh
@@ -1,3 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 
-ssh -i ../../ssh-key root@$(cat ip) "wget -O - http://$(cat ../native/ip)/performanceStats.sh 2>/dev/null | bash"
+make run 1>&2 &
+while ! ping $(cat ip) -c 1 1>&2
+do sleep 1
+done
+
+sleep 2
+
+./run-command.sh "wget -O - http://$(cat ../native/ip)/performanceStats.sh 2>/dev/null | bash"
+make shutdown 1>&2
+
+wait
