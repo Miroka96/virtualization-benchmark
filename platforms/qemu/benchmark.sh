c328bf36881ed7701be6130a82228bb3618bd4f0
Mirko Krause <mirko.krause@student.hpi.de>
2018-12-17 18:00:48 +0100

    prepared qemu benchmark

---
+++ platforms/qemu/benchmark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+make run 1>&2 &
+while ! ping $(cat ip) -c 1 1>&2
+do sleep 1
+done
+
+for file in $(ls ../../benchmarks/)
+do 	sudo cp ../../benchmarks/$file /var/www/html/
+	./run-command.sh "wget -O $file http://$(cat ../native/ip)/$file 2>/dev/null && chmod +x $file"
+done
+
+./run-command.sh "./nginx-prepare.sh"
+
+CLOUD=${PWD##*/}
+IP=$(cat ip)
+
+./run-command.sh "export CLOUD=$CLOUD && ./measure-all.sh"
+for result in $(./run-command.sh "ls $CLOUD-*.csv")
+do scp -i ../../ssh-key root@$(cat ip):$result .
+done
+
+OLDPWD=$(pwd)
+cd ../../benchmarks
+export CLOUD
+./measure-nginx.sh http://$IP/big.data
+mv $CLOUD-*.csv $OLDPWD
+
+
+make shutdown 1>&2
+wait
--- platforms/qemu/stat.sh
+++ platforms/qemu/stat.sh
@@ -5,8 +5,6 @@ while ! ping $(cat ip) -c 1 1>&2
 do sleep 1
 done
 
-sleep 2
-
 ./run-command.sh "wget -O - http://$(cat ../native/ip)/performanceStats.sh 2>/dev/null | bash"
 make shutdown 1>&2
 
